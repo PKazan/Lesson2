3ae51aa49e08bd9219491ad1ef018d22d9ecc644
Paweł Kazanecki <kazan87@gmail.com>
2021-01-15 20:13:54 +0100

    added tests for methods

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,5 +1,23 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println(greetPerson("Paweł"));
+        System.out.println(addTwoNumbers(1L, 2L));
+        System.out.println(divideTwoNumbers(10,0));
+    }
+    public static String greetPerson(String name) {
+        return "Hello " + name + "!";
+    }
+
+    public static long addTwoNumbers(long first, long second) {
+        return first+second;
+    }
+
+    public static int divideTwoNumbers(int first, int second) {
+        if (second == 0) {
+            return -1;
+        } else {
+            return first / second;
+        }
+
     }
 }
